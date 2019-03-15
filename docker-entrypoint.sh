77de4a25e045ab771055e7d003160dfc5274fee2
Nikita Gryzlov <nixel2007@gmail.com>
2019-03-15 19:22:10 +0300

    #4 set absolute out dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 sleep 5
 
-selenium-side-runner -s http://chromedriver:4444 --output-directory ../out /sides/*.side
+selenium-side-runner -s http://chromedriver:4444 --output-directory /root/out /sides/*.side
